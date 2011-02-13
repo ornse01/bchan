e8a48216b59ee31ba007c251cc34621d9d8ce549
ornse01 <ornse01@users.sourceforge.jp>
2011-02-13 12:36:52 +0000

    add NG by word condition in generate traydata.
    git-svn-id: http://svn.sourceforge.jp/svnroot/bchan/bchan/trunk@216 20a0b8eb-f62a-4a12-8fe1-b598822500fb

--- src/traydata.c
+++ src/traydata.c
@@ -1,7 +1,7 @@
 /*
  * traydata.c
  *
- * Copyright (c) 2010 project bchan
+ * Copyright (c) 2010-2010 project bchan
  *
  * This software is provided 'as-is', without any express or implied
  * warranty. In no event will the authors be held liable for any damages
@@ -128,6 +128,10 @@ LOCAL Bool dattraydata_isrescopytotray(datlayout_res_t *layout_res)
 		return False;
 	}
 	ok = datlayout_res_isenableidNG(layout_res);
+	if (ok == True) {
+		return False;
+	}
+	ok = datlayout_res_isenablewordNG(layout_res);
 	if (ok == True) {
 		return False;
 	}
